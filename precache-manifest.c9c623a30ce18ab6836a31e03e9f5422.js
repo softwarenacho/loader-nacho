e7050c4e0c617b84c71875847c31d49e5959e3ab
Nacho Betancourt <trsgarp@gmail.com>
2024-04-17 19:19:32 -0600

    Updates

---
+++ precache-manifest.c9c623a30ce18ab6836a31e03e9f5422.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "491b41e6d167a3da75b43cc70696b279",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b28883d97751e03c8df1",
+    "url": "./static/css/main.5cb948aa.chunk.css"
+  },
+  {
+    "revision": "09de333b7d8555eaa20e",
+    "url": "./static/js/2.497356b5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.497356b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b28883d97751e03c8df1",
+    "url": "./static/js/main.8529743b.chunk.js"
+  },
+  {
+    "revision": "0887ae981bf23d9c9965",
+    "url": "./static/js/runtime-main.d2e2a906.js"
+  }
+]);
